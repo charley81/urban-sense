73fcb0a7d600a20296556ec4e3d4f19f89ffc01a
charley81 <chrisharley81@gmail.com>
2019-02-27 13:04:55 -0500

    added media quiries and initial responsive styling

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,3 +2,19 @@
 function closeNav() {
   document.querySelector(".toggler").checked = false;
 }
+
+// Smooth Scrolling
+$(".menu-wrap a, .btn").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
